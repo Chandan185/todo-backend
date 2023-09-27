4ef9de27d03f4da93fbed1b53975912e0cea8746
Chandan Gupta <19chandan2002@gmail.com>
2023-09-27 13:17:43 +0530

    update usercontroller

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -68,8 +68,8 @@ const logout = asyncHandler(async (req, res, next) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
       httpOnly: true,
-    //   secure:true,
-      // sameSite:"None"
+      secure:true,
+      sameSite:"None"
   });
 
   res.status(200).json({
