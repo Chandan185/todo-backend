445410257585952cb97807d464229c0df88c8a4a
Chandan Gupta <19chandan2002@gmail.com>
2023-09-27 13:56:34 +0530

    update cookies

--- server.js
+++ server.js
@@ -14,8 +14,11 @@ app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", `${process.env.BASE_URL}`);
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
